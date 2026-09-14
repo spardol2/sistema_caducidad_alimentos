b1dcf3da15306daec7cf56841e4c51c724e1f1b3
samuel pardo <spardol2@ucentral.edu.co>
2026-09-14 10:43:22 -0500

    Corrige manager del usuario personalizado

--- backend/usuarios/models.py
+++ backend/usuarios/models.py
@@ -1,5 +1,31 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+
+
+class UsuarioManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('El email es obligatorio')
+
+        email = self.normalize_email(email)
+        usuario = self.model(email=email, **extra_fields)
+        usuario.set_password(password)
+        usuario.save(using=self._db)
+
+        return usuario
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('El superusuario debe tener is_staff=True')
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('El superusuario debe tener is_superuser=True')
+
+        return self.create_user(email, password, **extra_fields)
 
 
 class Usuario(AbstractUser):
@@ -21,6 +47,8 @@ class Usuario(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = UsuarioManager()
 
     def __str__(self):
         return f'{self.email} - {self.rol}'
