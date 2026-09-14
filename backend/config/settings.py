fe815b8948e73952c1bd9a4ad784d4b0387bb11b
samuel pardo <spardol2@ucentral.edu.co>
2026-09-14 09:48:16 -0500

    Configura usuario personalizado

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,8 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
     'usuarios',
-	
+    'productos',
+    'compras',
+    'notificaciones',
+    'supermercados',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +137,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'usuarios.Usuario'
--- backend/usuarios/models.py
+++ backend/usuarios/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Usuario(AbstractUser):
+    class Rol(models.TextChoices):
+        CONSUMIDOR = 'CONSUMIDOR', 'Consumidor'
+        SUPERMERCADO = 'SUPERMERCADO', 'Supermercado'
+        ADMIN = 'ADMIN', 'Administrador'
+
+    username = None
+    email = models.EmailField(unique=True)
+
+    rol = models.CharField(
+        max_length=20,
+        choices=Rol.choices,
+        default=Rol.CONSUMIDOR
+    )
+
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email} - {self.rol}'
