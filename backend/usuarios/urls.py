fcd1dead093a98491c5d15137a5742835d39951d
samuel pardo <spardol2@ucentral.edu.co>
2026-09-16 00:49:49 -0500

    Agrega inicio de sesión con JWT

--- backend/usuarios/urls.py
+++ backend/usuarios/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 from .views import RegistroUsuarioView
 
 
 urlpatterns = [
     path('registro/', RegistroUsuarioView.as_view(), name='registro'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
 ]
